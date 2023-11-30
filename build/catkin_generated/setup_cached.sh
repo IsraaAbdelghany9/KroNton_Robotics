eac80d00ae54dce3fcc955246d58ba12d21d8613
IsraaAbdelghany9 <israaabdelghany9@gmail.com>
2023-11-30 22:24:34 +0200

    create the pkg

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/israa/KroNton_Robotics/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/israa/KroNton_Robotics/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/israa/KroNton_Robotics/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/israa/KroNton_Robotics/src:$ROS_PACKAGE_PATH"
