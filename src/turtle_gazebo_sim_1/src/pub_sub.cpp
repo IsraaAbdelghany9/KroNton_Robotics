5c718af6e8be84cc40b3b7d25b21d8bd6f841efe
IsraaAbdelghany9 <israaabdelghany9@gmail.com>
2023-12-05 04:03:50 +0200

    update gazebo house code

--- src/turtle_gazebo_sim_1/src/pub_sub.cpp
+++ src/turtle_gazebo_sim_1/src/pub_sub.cpp
@@ -3,32 +3,45 @@
 #include <geometry_msgs/Twist.h>
 
 ros::Publisher cmd_vel_pub;
+float front_distance;
+float right_distance;
+float left_distance;
+
+geometry_msgs::Twist cmd_vel_msg;
 
 void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
-    float front_distance = msg->ranges[0];  // Distance in front of the robot
-    float right_distance = msg->ranges[90];  // Distance at the right side of the robot
-    float left_distance = msg->ranges[-90];  // Distance at the left side of the robot
+    front_distance = msg->ranges[0];  // Distance in front of the robot
+    right_distance = msg->ranges[90];  // Distance at the right side of the robot
+    left_distance = msg->ranges[-90];  // Distance at the left side of the robot
 
-    geometry_msgs::Twist cmd_vel_msg;
+    
 
     if (front_distance > 2.0) {
         // No obstacle in front, move forward
-        cmd_vel_msg.linear.x = 0.1;  // Set linear velocity for forward movement
+        cmd_vel_msg.linear.x = 0.2;  // Set linear velocity for forward movement
         cmd_vel_msg.angular.z = 0.0;  // Set angular velocity for turning
     } 
     else if(front_distance < 2.0)
     {
         // Obstacle in front, avoid it
-        if (right_distance > left_distance) {
+        if (right_distance > 2 && left_distance < 2) 
+        {
             // Obstacle is closer on the right side, turn left
-            cmd_vel_msg.linear.x = 0.1;  // Set linear velocity for stopping
+            cmd_vel_msg.linear.x = 0.0;  // Set linear velocity 
             cmd_vel_msg.angular.z = 0.5;  // Set angular velocity for left turn
             
         } 
-        else {
+        else if (right_distance < 2 && left_distance > 2)
+        {
             // Obstacle is closer on the left side, turn right
-            cmd_vel_msg.linear.x = -0.1;  // Set linear velocity for stopping
+            cmd_vel_msg.linear.x = 0.0;  // Set linear velocity 
             cmd_vel_msg.angular.z = -0.5;  // Set angular velocity for right turn
+        }
+        else 
+        {
+            // Obstacle is close on both sides, move backwards
+            cmd_vel_msg.linear.x = -0.2;  // Set linear velocity for backward movement
+            cmd_vel_msg.angular.z = 0.0;  // Set angular velocity for turning
         }
     }
 
