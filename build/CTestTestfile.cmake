51284c7404dcfc8199fe3b5475ee0d208ffd2258
IsraaAbdelghany9 <israaabdelghany9@gmail.com>
2024-02-11 00:03:00 +0200

    navigation_1

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,11 +5,13 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("module3/my_robot_slam")
 subdirs("ass_1")
 subdirs("custom_msg_pkg")
 subdirs("diff_robot_sim_1/diff_robot_description")
 subdirs("diff_robot_sim_1/diff_robot_gazebo")
 subdirs("divider_pkg")
+subdirs("module4/my_robot_navigation")
 subdirs("pub_sub_int")
 subdirs("topic_subscriber_pkg")
 subdirs("turtle_gazebo_sim_1")
---
+++ build/module4/my_robot_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/israa/KroNton_Robotics/src/module4/my_robot_navigation
+# Build directory: /home/israa/KroNton_Robotics/build/module4/my_robot_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
